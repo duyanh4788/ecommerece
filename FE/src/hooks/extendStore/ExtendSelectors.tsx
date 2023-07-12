0c6f89612bd8d8cf80aeddb9de298e428a04d155
duyanh4788 <duyanh4788@gmail.com>
2023-07-12 08:41:10 +0700

    w

--- FE/src/hooks/extendStore/ExtendSelectors.tsx
+++
@@ -1,7 +0,0 @@
-import * as AuthSelector from 'store/auth/shared/selectors';
-import { useSelector } from 'react-redux';
-
-export const ExtendSelectors = () => {
-  const refreshToken = useSelector(AuthSelector.selectRefreshToken);
-  return { refreshToken };
-};
